00a7e5a9a7dd26580bfb38cbd02fd1747d4c78ca
BPGT <lawhitenton62@gmail.com>
2025-08-09 04:16:51 -0500

    proper bepinex structure & enabling + disabling

--- Main.cs
+++ Main.cs
@@ -1,8 +1,11 @@
+using BepInEx;
 using GorillaNetworking;
 using Photon.Pun;
 
 namespace skibidi
 {
+    [Description("HauntedModMenu")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Main
     {
         static readonly bool rp = ControllerInputPoller.instance.rightControllerPrimaryButton;
@@ -19,14 +22,36 @@ namespace skibidi
 
         static readonly string SKIBIDI = "SKIBIDI";
 
+        static bool isEnabled = true;
+
         public static void Update()
         {
-            if (buttonsPressed)
+            if (isEnabled)
             {
-                PhotonNetwork.Disconnect();
+                if (buttonsPressed)
+                {
+                    PhotonNetwork.Disconnect();
 
-                PhotonNetworkController.Instance.AttemptToJoinSpecificRoom(SKIBIDI, JoinType.Solo);
+                    PhotonNetworkController.Instance.AttemptToJoinSpecificRoom(SKIBIDI, JoinType.Solo);
+                }
             }
         }
+
+        public static void OnEnable()
+        {
+            isEnabled = true;
+        }
+
+        public static void OnDisable()
+        {
+            isEnabled = false;
+        }
+    }
+
+    public class PluginInfo
+    {
+        public const string GUID = "com.linnea.bpgt.skibidi";
+        public const string Name = "skibidi";
+        public const string Version = "1.0.0";
     }
 }
